0aec1f2be0a2723c0d5976aca6192df0cd7c3d25
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2024-09-16 01:12:58 +0300

    Практика "Режимы открытия файлов"

---
+++ module_7_1.py
@@ -0,0 +1,46 @@
+import os
+
+
+class Product:
+
+    def __init__(self, name='', weight=0.0, category=''):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop(Product):
+    __file_name = 'products.txt'
+    if not os.path.isfile(__file_name):
+        file = open(__file_name, 'w')
+        file.close()
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        text = file.read()
+        file.close()
+        return text
+
+    def add(self, *products):
+        for i in products:
+            if str(i) not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(f'{i}\n')
+                file.close()
+            else:
+                print(f'Продукт {i} уже есть в магазине')
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
