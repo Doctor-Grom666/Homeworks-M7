5cecc6f7e700ebd2fbc14813977b965f66849494
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2024-09-19 00:21:49 +0300

    Практика "Форматирование строк"

---
+++ module_7_format_string.py
@@ -0,0 +1,25 @@
+team1_name = 'Мастера кода'
+team2_name = 'Волшебники данных'
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = score_1 + score_2
+time_avg = round((team1_time + team2_time) / (team1_num + team2_num), 2)
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    challenge_result = f'победа команды {team1_name}!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    challenge_result = f'победа команды {team2_name}!'
+else:
+    challenge_result = 'ничья!'
+
+print('В команде %s участников %s' % (team1_name, team1_num))
+print('Итого сегодня в командах участников: %s и %s' % (team1_num, team2_num))
+print('Команда {} решила задач: {}'.format(team2_name, score_2))
+print('{} решили задачи за {}'.format(team2_name, team2_time))
+print(f'Команды решили {score_1} и {score_2} задач')
+print(f'Результат битвы: {challenge_result}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
