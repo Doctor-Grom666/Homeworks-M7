62b320c851f88434c85776605bc23302359f936a
Doctor-Grom666 <127833238+Doctor-Grom666@users.noreply.github.com>
2024-09-24 22:03:33 +0300

    Практика "Файлы в ОС" - исправление

--- module_7_files_os.py
+++ module_7_files_os.py
@@ -5,15 +5,15 @@ directory = '.'
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(directory)
+        filepath = os.path.join(directory, file)
 
-        filetime = os.path.getmtime(directory)
+        filetime = os.path.getmtime(file)
 
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 
-        filesize = os.path.getsize(directory)
+        filesize = os.path.getsize(file)
 
-        parent_dir = os.path.dirname(directory)
+        parent_dir = os.path.dirname(file)
 
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
               f'Родительская директория: {parent_dir}')
